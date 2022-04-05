6cbb1e1ac48651b3ec0811ad992eff16b1d37cee
Jake Rhodes <jake.rhodes@usu.edu>
2022-04-05 13:57:30 -0600

    Updated name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='rfphate',
+    name='RF-PHATE',
     version='0.0.1',
     author='Jake Rhodes',
     author_email='jake.rhodes@usu.edu',
